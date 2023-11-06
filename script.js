4dfa31ee4cae9da92b65b814bb2a9acd7f0198bc
renatoserranoo <renatoserranoribeiro@gmail.com>
2023-11-06 15:52:53 -0300

    css improve

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.sr = ScrollReveal({ reset:true});
-ScrollReveal({ distance: '40px' });
-sr.reveal('.desenho', {duration : 1500, origin: 'right'})
-sr.reveal('.conteudo', {duration : 1500, origin: 'left'})
-sr.reveal('principal_button-github', {duration : 1500,  origin: 'left'})
-sr.reveal('.desenho-bg', {duration : 1500,  origin: 'left'})
-sr.reveal('.sobre-mim', {duration : 1500, origin:'right'})
-sr.reveal('.projetos-itens', {duration : 1500, origin:'bottom'})
-sr.reveal('.project', {duration : 1500, origin:'bottom'})
-sr.reveal('.github-link', {duration : 1500, origin:'bottom'})
+ScrollReveal({ distance: '70px' });
+sr.reveal('.desenho', {duration : 1300, origin: 'right'})
+sr.reveal('.conteudo', {duration : 1300, origin: 'left'})
+sr.reveal('principal_button-github', {duration : 1300,  origin: 'left'})
+sr.reveal('.desenho-bg', {duration : 1300,  origin: 'left'})
+sr.reveal('.sobre-mim', {duration : 1300, origin:'right'})
+sr.reveal('.projetos-itens', {duration : 1300, origin:'bottom'})
+sr.reveal('.project', {duration : 1300, origin:'bottom'})
+sr.reveal('.github-link', {duration : 1300, origin:'bottom'})
